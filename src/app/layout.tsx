9c60a2d6049e09445c72fac7caa1807b72464002
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-01-31 17:57:41 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${chelsea_market.variable} ${dmSans.variable}`}>
-        {/* <Preloader /> */}
+      
 
         <ClientPreloader />
         {children}
