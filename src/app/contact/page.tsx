13756ec7c4157e200ab5cb67a99bee60c7d888c9
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-02-06 17:59:41 +0530

    on page seo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,6 +4,17 @@ import PageBanner from "@/components/PageBanner";
 import DanboxLayout from "@/layout/DanboxLayout";
 import { useState } from "react";
 import Swal from "sweetalert2";
+import { generateSeo } from "@/lib/Seo";
+import { Metadata } from "next";
+
+
+export const generateMetadata = (): Metadata =>
+  generateSeo({
+    title: "Contact Scrap Buyers Chennai | Scrap Dealers Near Me",
+    description: "Contact scrap buyers near me in Chennai for instant scrap pickup call +91 76395 82802 today for best scrap prices",
+    path: "/",
+    images: '/assets/img/waste-images/metal-waste/img-1.jpg',
+  });
 
 
 const CausesPage = () => {
--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -5,6 +5,17 @@ import PageBanner from "@/components/PageBanner";
 // import { Service1 } from "@/components/Service";
 import ServiceContent from "@/components/ServiceContent";
 import DanboxLayout from "@/layout/DanboxLayout";
+import { generateSeo } from "@/lib/Seo";
+import { Metadata } from "next";
+
+
+export const generateMetadata = (): Metadata =>
+  generateSeo({
+    title: "Scrap Collection Services Chennai | Scrap Buyers Near Me",
+    description: "Professional scrap collection near me in Chennai for metal scrap electronic scrap battery scrap with instant pickup service",
+    path: "/",
+    images: '/assets/img/waste-images/metal-waste/img-1.jpg',
+  });
 
 const CausesPage = () => {
   return (
